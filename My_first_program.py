5ef67f7daa49728fe59a80b6235aefbcedbaca59
ZacHarper-313 <zac.harper5002@gmail.com>
2025-06-09 13:01:23 -0400

    This is my first program!

---
+++ My_first_program.py
@@ -0,0 +1,23 @@
+#print word
+print ('hello world')
+#assigning variable
+first_number, second = 10, 12
+print(first_number+second)
+#assign value to site_name variable
+site_name='programiz.pro'
+print(site_name)
+#output: programiz.pro
+a,b,c=5,3.2,'hello'
+print(a) #prints 5
+print(b) #prints 3.2
+print(c) #prints hello
+print(a+b)
+#function to add two numbers
+#step get first number
+number_1=int(input('enter 1st number for sum '))
+# convert number 1 to intenger
+#step get second number
+number_2=int(input('enter 2nd number for sum '))
+#store sum from 1st and second number
+sum=number_1+number_2
+print(number_1+number_2)
